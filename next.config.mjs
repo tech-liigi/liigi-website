423d1fa32faf14c7346ec4c4c11591914e2325e7
MAKAR190 <lutskyimakar@gmail.com>
2024-04-19 15:14:55 +0200

    pushed

--- next.config.mjs
+++ next.config.mjs
@@ -9,9 +9,9 @@ const nextConfig = {
         ],
     },
     env: {
-        NEXT_PUBLIC_SANITY_PROJECT_ID: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-        NEXT_PUBLIC_SANITY_DATASET: process.env.NEXT_PUBLIC_SANITY_DATASET,
-        SANITY_SECRET_TOKEN: process.env.SANITY_SECRET_TOKEN
+        NEXT_PUBLIC_SANITY_PROJECT_ID: "op15swdk",
+        NEXT_PUBLIC_SANITY_DATASET: "production",
+        SANITY_SECRET_TOKEN: "skfIX2mLZsesL8DxA7UbMKvxgFFCqXNtZWbPKdVawCYbXY24E9q5whUWzJZrHqDOR7WytfWv7n6l5xCLgmQUI43RA7rKzGRRqAbJqUh3x8mWv985ZMaDhb8hIA4SCkSxVqprryd5cXHUomoYA13QA23Sb9ZRZfQdtrNeEHRgojakLfCBn0fD"
     },
 };
 
